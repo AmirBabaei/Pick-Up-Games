54cb7afea6dda5524f7cf2300af163f97048b16a
Amir <babaei.a94@gmail.com>
2018-10-12 01:42:07 -0700

    Initial Commit

---
+++ Pick-Up Games/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Pick-Up Games
+//
+//  Created by Amir Babaei on 10/12/18.
+//  Copyright © 2018 Amir Babaei. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view, typically from a nib.
+  }
+
+
+}
+
---
+++ Pick-Up GamesTests/Pick_Up_GamesTests.swift
@@ -0,0 +1,34 @@
+//
+//  Pick_Up_GamesTests.swift
+//  Pick-Up GamesTests
+//
+//  Created by Amir Babaei on 10/12/18.
+//  Copyright © 2018 Amir Babaei. All rights reserved.
+//
+
+import XCTest
+@testable import Pick_Up_Games
+
+class Pick_Up_GamesTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
