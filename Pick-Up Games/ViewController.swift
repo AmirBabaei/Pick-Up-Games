d2025fde37a41fe3c202e1458bc733bf15c0d3d4
raul pineda <lito@rauls-imac.local>
2018-10-15 08:36:44 -0700

    test

--- Pick-Up Games/ViewController.swift
+++ Pick-Up Games/ViewController.swift
@@ -10,7 +10,7 @@ import UIKit
 
 class ViewController: UIViewController {
   override func viewDidLoad() {
-    print("Amir test")
+    print("Amir test, raul test")
     super.viewDidLoad()
     // Do any additional setup after loading the view, typically from a nib.
   }
