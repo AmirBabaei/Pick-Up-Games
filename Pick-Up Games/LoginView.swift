587188842f098042766dd1a9f6f2f86cea889a6b
Amir <babaei.a94@gmail.com>
2018-10-19 09:31:12 -0700

    Textfields are added to RegisterView

--- Pick-Up Games/LoginView.swift
+++ Pick-Up Games/LoginView.swift
@@ -8,12 +8,10 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class LoginView: UIViewController {
   override func viewDidLoad() {
     super.viewDidLoad()
     // Do any additional setup after loading the view, typically from a nib.
-  }
-  @IBAction func unwindToLogin(segue: UIStoryboardSegue){
   }
 
 }
---
+++ Pick-Up Games/RegisterView.swift
@@ -0,0 +1,30 @@
+//
+//  RegisterView.swift
+//  Pick-Up Games
+//
+//  Created by Amir Babaei on 10/18/18.
+//  Copyright © 2018 Amir Babaei. All rights reserved.
+//
+
+import UIKit
+
+class RegisterView: UIViewController {
+
+  @IBOutlet var UsernameTF: UITextField!
+  @IBOutlet var EmailTF: UITextField!
+  @IBOutlet var PasswordTF: UITextField!
+  @IBOutlet var CONPassTF: UITextField!
+  
+  
+  
+  override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+  
+  @IBAction func RegisterPressed(_ sender: Any) {
+  }
+  
+
+}
