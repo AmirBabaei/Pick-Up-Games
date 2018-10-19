90de268a60740ff73f53488adaaff1413455c533
Amir <babaei.a94@gmail.com>
2018-10-19 10:45:36 -0700

    Registercontroller view buttons and fields connected. Firebase setup

--- Pick-Up Games/RegisterViewController.swift
+++ Pick-Up Games/RegisterViewController.swift
@@ -10,14 +10,23 @@ import UIKit
 
 class RegisterViewController: UIViewController {
 
-    override func viewDidLoad() {
+  @IBOutlet var UserNameTF: UITextField!
+  
+  @IBOutlet var EmailTF: UITextField!
+  
+  @IBOutlet var PasswordTf: UITextField!
+  
+  @IBOutlet var ConPassTf: UITextField!
+  override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
     }
     
 
-    /*
+  @IBAction func RegisterPressed(_ sender: Any) {
+  }
+  /*
     // MARK: - Navigation
 
     // In a storyboard-based application, you will often want to do a little preparation before navigation
