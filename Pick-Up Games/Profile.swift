9bfd0e890ee50437bc2d4e0b7e25b1f27d01612b
David Otwell <dotwell@davids-imac.local>
2018-11-07 10:51:58 -0800

    Code is broken but I am heading out to meet up so I'm commiting.

--- Pick-Up Games/Profile.swift
+++ Pick-Up Games/Profile.swift
@@ -15,5 +15,22 @@ class Profile: UIViewController {
         super.viewDidLoad()
     }
     
+    @IBOutlet weak var Username: UILabel!
+    @IBOutlet weak var Email: UILabel!
+    @IBOutlet weak var Name: UILabel!
+    @IBOutlet weak var Age: UILabel!
     
+    let REF_PROF = Database.database().reference().child("users")
+    REF_PROF.observeSingleEvent(of: .value) { (profUserSnapshot) in
+        guard let feedEventSnapshot = feedEventSnapshot.children.allObjects as? [DataSnapshot] else { return }
+    
+        for pug in feedEventSnapshot {
+            let address = pug.childSnapshot(forPath: "EventLocation").value as! String
+            let sport = pug.childSnapshot(forPath: "EventType").value as! String
+            let players = pug.childSnapshot(forPath: "EventParticipant_Limit").value as! String
+            let name = pug.childSnapshot(forPath: "EventCreator_UserName").value as! String
+            funcEventArray.append(pug)
+        }
+        handler(funcEventArray)
+    }
 }
