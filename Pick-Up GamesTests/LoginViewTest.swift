b2c273f34c98293980b25ff3e6f1dff00ab464af
user146820 <user146820@74-80-245-200.ic2net.net>
2018-11-09 12:09:23 -0800

    Basic testing setup

---
+++ Pick-Up GamesTests/LoginViewTest.swift
@@ -0,0 +1,22 @@
+//
+//  LoginViewTest.swift
+//  Pick-Up GamesTests
+//
+//  Created by user146820 on 11/9/18.
+//  Copyright © 2018 Amir Babaei. All rights reserved.
+//
+
+import XCTest
+
+class LoginViewTest: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+
+}
