ec986619e4497ebc24f8c89b081daf2ee9245f2c
David Otwell <dotwell@davids-imac.local>
2018-11-09 10:58:32 -0800

    Committing safe files at a time to fix a mistake

--- Pick-Up Games/Profile.swift
+++ Pick-Up Games/Profile.swift
@@ -6,11 +6,12 @@
 //  Copyright © 2018 Amir Babaei. All rights reserved.
 //
 
-import Foundation
 import UIKit
 import Firebase
+import CoreLocation
 
 class Profile: UIViewController {
+    
     override func viewDidLoad() {
         super.viewDidLoad()
     }
@@ -19,18 +20,22 @@ class Profile: UIViewController {
     @IBOutlet weak var Email: UILabel!
     @IBOutlet weak var Name: UILabel!
     @IBOutlet weak var Age: UILabel!
-    
-    let REF_PROF = Database.database().reference().child("users")
-    REF_PROF.observeSingleEvent(of: .value) { (profUserSnapshot) in
-        guard let feedEventSnapshot = feedEventSnapshot.children.allObjects as? [DataSnapshot] else { return }
-    
-        for pug in feedEventSnapshot {
-            let address = pug.childSnapshot(forPath: "EventLocation").value as! String
-            let sport = pug.childSnapshot(forPath: "EventType").value as! String
-            let players = pug.childSnapshot(forPath: "EventParticipant_Limit").value as! String
-            let name = pug.childSnapshot(forPath: "EventCreator_UserName").value as! String
-            funcEventArray.append(pug)
+
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        getBasicInfo()
+    }
+}
+
+extension Profile {
+    func getBasicInfo() {
+        let userID = Auth.auth().currentUser?.uid
+        let REF_PROF = Database.database().reference().child("users").child(userID!)
+        REF_PROF.observeSingleEvent(of: .value) { (profUserSnapshot) in
+            self.Username.text = profUserSnapshot.childSnapshot(forPath: "username").value as? String
+            self.Name.text = profUserSnapshot.childSnapshot(forPath: "Full Name").value as? String
+            self.Email.text = Auth.auth().currentUser?.email
+            self.Age.text = profUserSnapshot.childSnapshot(forPath: "Age").value as? String
         }
-        handler(funcEventArray)
     }
 }
