51b0eef16a49d35d00799213f815ae0c5f5f51b7
user146820 <user146820@74-80-245-200.ic2net.net>
2018-11-29 17:05:55 -0800

    Created login and check if logged in functions for testing. These test should work unless issues are being caused by the pod file

--- Pick-Up GamesUITests/Pick_Up_GamesUITests.swift
+++ Pick-Up GamesUITests/Pick_Up_GamesUITests.swift
@@ -53,6 +53,7 @@ class Pick_Up_GamesUITests: XCTestCase {
             let element2 = window.children(matching: .other).element(boundBy: 1).children(matching: .other).element
             XCTAssertTrue(element2.children(matching: .textField).element.exists)
             XCTAssertTrue(element2.children(matching: .secureTextField).element.exists)
+            loginToApp(app: app)
         }
         
         
@@ -60,8 +61,9 @@ class Pick_Up_GamesUITests: XCTestCase {
     func testFEED(){
         
         let app = XCUIApplication()
-        XCTAssertTrue(app.children(matching: .window).element(boundBy: 0).children(matching: .other).element.children(matching: .other).element.children(matching: .other).element.children(matching: .other).element.children(matching: .other).element.exists)
+        checkIfLoggedIn(app: app)
         
+        sleep(10)
         let feedNavigationBar = app.navigationBars["feed"]
         XCTAssertTrue(feedNavigationBar.buttons["Profile"].exists)
         feedNavigationBar.buttons["Profile"].tap()
@@ -69,6 +71,74 @@ class Pick_Up_GamesUITests: XCTestCase {
         feedNavigationBar.buttons["Add"].tap()
         app.navigationBars["createEvent"].buttons["feed"].tap()
                 
-            }
-
+        }
+    
+    func testProfile(){
+        
+    }
+    func checkIfLoggedIn(app:XCUIElement){
+        let getStartedButton = app.buttons["GET STARTED"]
+        if(getStartedButton.exists)
+        {
+            loginToApp(app: app)
+        }
+    }
+    func loginToApp(app:XCUIElement){
+        let gotologinviewElementsQuery = app.otherElements.containing(.image, identifier:"gotoLoginView")
+        let textField = gotologinviewElementsQuery.children(matching: .textField).element
+        let app = XCUIApplication()
+        textField.tap()
+        let pKey = app/*@START_MENU_TOKEN@*/.keys["p"]/*[[".keyboards.keys[\"p\"]",".keys[\"p\"]"],[[[-1,1],[-1,0]]],[0]]@END_MENU_TOKEN@*/
+        let sKey = app.keys["s"]
+        let mKey = app.keys["m"]
+        let wKey = app/*@START_MENU_TOKEN@*/.keys["w"]/*[[".keyboards.keys[\"w\"]",".keys[\"w\"]"],[[[-1,1],[-1,0]]],[0]]@END_MENU_TOKEN@*/
+        let oKey = app/*@START_MENU_TOKEN@*/.keys["o"]/*[[".keyboards.keys[\"o\"]",".keys[\"o\"]"],[[[-1,1],[-1,0]]],[0]]@END_MENU_TOKEN@*/
+        let eKey = app.keys["e"]
+        let tKey = app.keys["t"]
+        let cKey = app.keys["c"]
+        let dKey = app.keys["d"]
+        let moreKey = app/*@START_MENU_TOKEN@*/.keys["more"]/*[[".keyboards",".keys[\"more, numbers\"]",".keys[\"more\"]"],[[[-1,2],[-1,1],[-1,0,1]],[[-1,2],[-1,1]]],[0]]@END_MENU_TOKEN@*/
+        let atKey = app/*@START_MENU_TOKEN@*/.keys["@"]/*[[".keyboards.keys[\"@\"]",".keys[\"@\"]"],[[[-1,1],[-1,0]]],[0]]@END_MENU_TOKEN@*/
+        let moreKey2 = app/*@START_MENU_TOKEN@*/.keys["more"]/*[[".keyboards",".keys[\"more, letters\"]",".keys[\"more\"]"],[[[-1,2],[-1,1],[-1,0,1]],[[-1,2],[-1,1]]],[0]]@END_MENU_TOKEN@*/
+        let periodKey2 = app/*@START_MENU_TOKEN@*/.keys["."]/*[[".keyboards.keys[\".\"]",".keys[\".\"]"],[[[-1,1],[-1,0]]],[0]]@END_MENU_TOKEN@*/
+        tKey.tap()
+        eKey.tap()
+        sKey.tap()
+        tKey.tap()
+        
+        moreKey.tap()
+        atKey.tap()
+        moreKey2.tap()
+        
+        tKey.tap()
+        eKey.tap()
+        sKey.tap()
+        tKey.tap()
+        
+        moreKey.tap()
+        periodKey2.tap()
+        moreKey2.tap()
+        
+        cKey.tap()
+        oKey.tap()
+        mKey.tap()
+        
+        
+        
+        
+        let secureTextField = gotologinviewElementsQuery.children(matching: .secureTextField).element
+        secureTextField.tap()
+        secureTextField.tap()
+        
+        pKey.tap()
+        app/*@START_MENU_TOKEN@*/.keys["a"]/*[[".keyboards.keys[\"a\"]",".keys[\"a\"]"],[[[-1,1],[-1,0]]],[0]]@END_MENU_TOKEN@*/.tap()
+        sKey.tap()
+        sKey.tap()
+        wKey.tap()
+        oKey.tap()
+        app/*@START_MENU_TOKEN@*/.keys["r"]/*[[".keyboards.keys[\"r\"]",".keys[\"r\"]"],[[[-1,1],[-1,0]]],[0]]@END_MENU_TOKEN@*/.tap()
+        dKey.tap()
+        
+        app.buttons["GET STARTED"].tap()
+    }
 }
